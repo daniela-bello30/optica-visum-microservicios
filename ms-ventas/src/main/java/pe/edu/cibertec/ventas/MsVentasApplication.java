8ea594d2aa2c0b3b5f683993d43bc7414d9e687d
danielabellgut-stack <daniela.bellgut@gmail.com>
2026-02-11 15:44:53 -0500

    Avance del proyecto

--- ms-ventas/src/main/java/pe/edu/cibertec/ventas/MsVentasApplication.java
+++ ms-ventas/src/main/java/pe/edu/cibertec/ventas/MsVentasApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class VentasApplication {
+public class MsVentasApplication {
     public static void main(String[] args) {
-        SpringApplication.run(VentasApplication.class, args);
+        SpringApplication.run(MsVentasApplication.class, args);
     }
 }
